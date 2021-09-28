5ab317bf1c145a42f36b1ed04579c0e03fa9e457
Wypalacz <wypalaczrun@gmail.com>
2021-09-27 23:40:23 -0300

    feat: retirando as instancias desnecessarias do mysql

--- comandos/ip.js
+++ comandos/ip.js
@@ -3,16 +3,33 @@ const MysqlManager = require('../database.js');
 const config = require('../config.js')
 
 exports.run = async (client, message, args) => {
-  const phoenix = new MysqlManager(client, config);
   const erro = new MessageEmbed()
     .setTitle(`Uso incorreto`)
     .setDescription(`!ip <script> <ip>`)
     .setColor(0x00ae86)
     .setTimestamp();
   if (!args[0] || !args[1]) return message.channel.send(erro);
-  if(phoenix.checkScript(message,args[0])) {
-    phoenix.updateScript(message,args[0],args[1])
-  }
+  client.db.query(
+    `SELECT script, ip FROM subs WHERE discordid = "${message.author.id}" AND script = "${args[0]}" `,
+    async (err, rows) => {
+      if (!rows || rows.length === 0) {
+        message.channel.send(
+          `:x: Você não possui o script **${args[0]}**, contate um administrador caso ache que isso seja um engano.`
+        );
+      } else {
+        client.db.query(
+          `UPDATE subs SET ip = "${args[1]}" WHERE discordid = "${message.author.id}" AND script = "${args[0]}"`,
+          async (err, rows) => {
+            if (rows.affectedRows >= 1) {
+              return message.channel.send(
+                `:white_check_mark: **IP atualizado para o script** **__${args[0]}__** `
+              );
+            }
+          }
+        );
+      }
+    }
+  );
 };
 
 exports.config = {
--- comandos/subs.js
+++ comandos/subs.js
@@ -3,8 +3,31 @@ const MysqlManager = require('../database.js');
 const config = require('../config.js');
 
 exports.run = async (client, message, args) => {
-    const phoenix = new MysqlManager(client, config);
-    phoenix.checkScripts(message)
+    client.db.query(
+        `SELECT script, ip FROM subs WHERE discordid = "${message.author.id}"`,
+        async (err, rows) => {
+          if (err) throw err;
+          if (!rows || rows.length === 0) {
+            return message.channel.send(
+              `:negative_squared_cross_mark: **Você não possui nenhum script!** `
+            );
+          }
+          const embed = new MessageEmbed()
+            .setTitle(`Assinaturas (${rows.length})`)
+            .setColor(0x00ae86)
+            .addField(
+              `Nome`,
+              rows.map((x) => x.script),
+              true
+            )
+            .addField(
+              `IP`,
+              rows.map((x) => (x.ip ? x.ip : "Nenhum")),
+              true
+            );
+          return message.channel.send(embed);
+        }
+      );
 }
 
 exports.config = {
